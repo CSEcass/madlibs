b5734bd999926e1eaa352c913261f1b9eb493365
CSEcass <cassidy.reyes@csedge.org>
2024-06-25 11:55:39 -0700

    1st MadLibs para & 8 user inputs

--- madlibs.py
+++ madlibs.py
@@ -1,2 +1,15 @@
 # MadLib by Cass
-print('Here is my first commit :D')
+print('----MadLibs----')
+
+noun = input('Enter a noun: ')
+verb = input('Enter a verb: ')
+verb2 = input('Give me a better one: ')
+noun2 = input('Now give me a NOUN: ')
+adjective = input('I NEED an adjective: ')
+adjective2 = input('Anotha 1: ')
+adverb = input('I lie. I want adverbs more(ugly sobbing):')
+adverb2 = input('Got any DECENT(not like the other one u just entered... yikes), adverbs: ')
+Feel = input('how do u feel so far(i do NOT care): ')
+ 
+print(f'In a surprising turn of events, {noun} transformed into a {verb} battleground for \nWorld War III, leaving behind a trail of destruction and chaos with everyone {verb2} in its wake.\nThe once {adverb} {adjective} land now echoed with the sounds of {adverb2} and {adjective2}. \nAs the dust settled,\nthe protagonist found themselves grappling with overwhelming emotions and deep-seated trauma.\nThey felt {Feel}, Seeking solace and {noun2}, they embarked on a\njourney of intensive therapy to confront their clinical depression and make sense\nof the turmoil that had engulfed their beloved {noun}.')
+# The end
